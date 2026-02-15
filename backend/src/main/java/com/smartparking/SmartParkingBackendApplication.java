02974ae1e3d648b220c6ef7db063c10576d4996c
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2026-02-15 18:06:35 +0530

    admin details shifted to env var

--- backend/src/main/java/com/smartparking/SmartParkingBackendApplication.java
+++ backend/src/main/java/com/smartparking/SmartParkingBackendApplication.java
@@ -3,6 +3,7 @@ package com.smartparking;
 import com.smartparking.entity.Role;
 import com.smartparking.entity.User;
 import com.smartparking.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SmartParkingBackendApplication {
+    @Value("${admin.email}")
+    private String adminEmail;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
 
     public static void main(String[] args) {
         SpringApplication.run(SmartParkingBackendApplication.class, args);
@@ -19,11 +26,11 @@ public class SmartParkingBackendApplication {
     @Bean
     CommandLineRunner commandLineRunner(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
-            if (!userRepository.existsByEmail("admin@smartparking.com")) {
+            if (!userRepository.existsByEmail(adminEmail)) {
                 User admin = User.builder()
                         .name("Admin User")
-                        .email("admin@smartparking.com")
-                        .password(passwordEncoder.encode("admin123"))
+                        .email(adminEmail)
+                        .password(passwordEncoder.encode(adminPassword))
                         .role(Role.ADMIN)
                         .build();
                 userRepository.save(admin);
